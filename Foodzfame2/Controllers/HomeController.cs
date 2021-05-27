07d35dc4516c78e9d679333b61faaa130b7a7a7f
Prashant Kumar <Prashant.Kumar@zapcg.com>
2021-05-27 17:08:49 +0530

    Removing use of GraphQL from Home

--- Foodzfame2/Controllers/HomeController.cs
+++ Foodzfame2/Controllers/HomeController.cs
@@ -2,15 +2,11 @@
 using Foodzfame.Data.FoodzfameContext;
 using Foodzfame.Utility;
 using Foodzfame2.Models;
-using Foodzfame2.Models.GraphQLResponse;
-using GraphQL;
-using GraphQL.Client.Abstractions;
-using GraphQL.Client.Http;
-using GraphQL.Client.Serializer.Newtonsoft;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -24,90 +20,53 @@ namespace Foodzfame2.Controllers
         private readonly ILogger<HomeController> _logger;
         IDistributedCache _memoryCache;
         FoodzfameContext _dbContext;
-        GraphQLHttpClient graphQLClient;
-        public HomeController(ILogger<HomeController> logger, IDistributedCache distributedCache, FoodzfameContext context, GraphQLHttpClient graphQLHttpClient)
+        public HomeController(ILogger<HomeController> logger, IDistributedCache distributedCache, FoodzfameContext context)
         {
-            graphQLClient = graphQLHttpClient;
             _logger = logger;
             _memoryCache = distributedCache;
             _dbContext = context;
         }
         [OutputCache(Duration = 14400)]
-        public async Task<IActionResult> IndexAsync()
+        public IActionResult Index()
         {
-            // To use NewtonsoftJsonSerializer, add a reference to NuGet package GraphQL.Client.Serializer.Newtonsoft
-            //var graphQLClient = new GraphQLHttpClient("https://localhost:44352/graphql/", new NewtonsoftJsonSerializer());
-            var recipeRequest = new GraphQLRequest
+            var mostPopular = _memoryCache.Get("PopularPosts");
+            var popularPosts = new List<Dish>();
+            if (mostPopular == null)
             {
-                Query = @"{
-                              dishes
-                              {
-                                nodes{
-                                  id,
-                                  dishName,
-                                  desc,
-                                  addedBy,
-                                  cookingTime,
-                                  dishCategory{
-                                                category,
-                                                des
-                                              }
-                                }
-                              }
-                            }"
-            };
-            var categoryRequest = new GraphQLRequest
-            {
-                Query= @"{
-                          categories{
-                            nodes{
-                              id,
-                              category1,
-                              des,
-                              subCategories{
-                                id,
-                                category,
-                                des,
-                                dishes{
-                                        id
-                                        }
-                              }
-                            }
-                          }
-                        }"
-            };
-
-            var recipeResult = await graphQLClient.SendQueryAsync(recipeRequest, () => new GraphQLResponseVm());
-            //ViewBag.popularPosts = (List<Dish>)Utils.ByteArrayToObject(_memoryCache.Get("PopularPosts"));
-            ViewBag.popularPosts = recipeResult.Data.dishes.nodes.AsEnumerable().OrderByDescending(x=>x.Likes).Take(8).ToList();
-            ViewBag.recipeByCategory = recipeResult.Data.dishes.nodes.AsEnumerable().OrderByDescending(n => Guid.NewGuid()).Take(12).ToList();
-            var categoryResult= await graphQLClient.SendQueryAsync(categoryRequest, () => new GraphQLResponseVm());
-            var Category = categoryResult.Data.categories.nodes;
-            var SubCategories = categoryResult.Data.categories.nodes.SelectMany(x => x.SubCategories);
+                popularPosts = _dbContext.Dishes.AsEnumerable().OrderByDescending(n => n.Likes).Take(8).ToList();
+                var cacheOptions = new DistributedCacheEntryOptions
+                {
+                    AbsoluteExpiration = DateTime.Now.AddDays(7)
+                };
+                _memoryCache.Set("PopularPosts", Utils.ObjectToByteArray(popularPosts), cacheOptions);
+            }
+            ViewBag.popularPosts = (List<Dish>)Utils.ByteArrayToObject(_memoryCache.Get("PopularPosts"));
+            ViewBag.recipeByCategory = _dbContext.Dishes.Include(x => x.DishCategory).AsEnumerable().OrderByDescending(n => Guid.NewGuid()).Take(12).ToList();
+            var Category = _dbContext.Categories.Include(x => x.SubCategories).ToList();
             var Counters = new Counters();
             Counters.counts = new List<CategoryRecipeCounts>();
             foreach (var item in Category)
             {
                 int counter = 0;
-                foreach(var subItem in item.SubCategories)
+                foreach (var subItem in item.SubCategories)
                 {
-                    counter += SubCategories.FirstOrDefault(x => x.Id == subItem.Id).Dishes.ToList().Count();
+                    counter += _dbContext.SubCategories.Include(x => x.Dishes).FirstOrDefault(x => x.Id == subItem.Id).Dishes.ToList().Count();
                 }
-                
+
                 Counters.counts.Add(new CategoryRecipeCounts()
                 {
                     SubCatCount = item.SubCategories.ToList().Count(),
-                    CategoryName=item.Category1,
-                    RecipeCount= counter,
-                    CategoryId=item.Id
+                    CategoryName = item.Category1,
+                    RecipeCount = counter,
+                    CategoryId = item.Id
 
                 });
             }
             ViewBag.Category = Counters;
             ViewBag.Blogs = _dbContext.Blogs.Take(6).ToList();
             List<string> tags = new List<string>();
-            tags.AddRange(Category.Select(s=>s.Category1));
-            tags.AddRange(((List<Dish>)ViewBag.popularPosts).Select(s=>s.DishName));
+            tags.AddRange(Category.Select(s => s.Category1));
+            tags.AddRange(((List<Dish>)ViewBag.popularPosts).Select(s => s.DishName));
             tags.Add("foodblog food blog recipe recipes spicy tangy indianfood indianrecipe indiancuisine foodcategory category Every day recipe fastfood");
             List<KeyValuePair<string, string>> attr = new List<KeyValuePair<string, string>>();
             attr.Add(new KeyValuePair<string, string>("title", "Foodzfame a food blog"));
@@ -159,14 +118,15 @@ namespace Foodzfame2.Controllers
         }
         public IActionResult Subscribe(string email)
         {
-            if(_dbContext.SubScriptions.Any(x=>x.Email==email))
+            if (_dbContext.SubScriptions.Any(x => x.Email == email))
             {
                 return Json(0);
             }
             _dbContext.SubScriptions.Add(
-                new SubScription() { 
-                Email=email,
-                AddedDate=DateTime.Now
+                new SubScription()
+                {
+                    Email = email,
+                    AddedDate = DateTime.Now
                 }
                 );
             _dbContext.SaveChanges();
